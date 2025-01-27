e3495bae5ae71c7140304a7d28cefc3dbc98ab50
simran-ss-sandhu <simran.ss.sandhu23@gmail.com>
2025-01-27 21:00:41 +0000

    feat(colour): improve HSV-based colour matching accuracy
    - Enhanced colour matching accuracy by refining HSV boundaries and adjusting the if conditions for colour range checks.
    - These changes result in more precise recognition of colours and reduce false matches (especially between red and orange)

--- src/rubiks_cube_state_recognition/cube_capture/ColourFinder.py
+++ src/rubiks_cube_state_recognition/cube_capture/ColourFinder.py
@@ -12,13 +12,11 @@ class Colour:
 class ColourFinder:
     def __init__(self):
         self.colours = [
-            Colour('r', 177.5, 6.24),  # red object. Special case -> (larger than 177.5) or (between 0 and 5)
-            Colour('o', 6.26, 22.74),  # orange object
+            Colour('r', 177.5, 5.65),  # red object. Special case -> (larger than 177.5) or (between 0 and 5)
+            Colour('o', 5.65, 22.76),  # orange object
             Colour('y', 22.76, 35.24),  # yellow object
             Colour('g', 35.26, 90.24),  # green object
             Colour('b', 90.26, 120)]  # blue object
-        self.non_white_min_saturation = 127.5  # the minimum saturation required for a colour to not be white
-        self.non_white_min_brightness = 127.5  # the maximum brightness allowed for a colour to not be white
         self.white_max_saturation = 70  # the maximum saturation allowed for a colour to be white
         self.white_min_brightness = 120  # the minimum brightness required for a colour to be white
         self.frame = None
@@ -49,21 +47,16 @@ class ColourFinder:
     def __find_hsb_colour_name(self):
         hue, saturation, brightness = self.dominant_HSB  # splits the HSB array into separate variables
 
-        # checks for red, orange, yellow, green and blue colours
-        if (saturation >= self.non_white_min_saturation) and (brightness >= self.non_white_min_brightness):  # the brightness and saturation don't satisfy the ranges for grey/black
-
-            # special case check for the colour red
-            if (hue >= self.colours[0].minimum_hue) or (hue <= self.colours[0].maximum_hue):  # the hue is within the red hue range
-                return 'r'
-
+        if brightness >= self.white_min_brightness and saturation <= self.white_max_saturation:
+            return 'w'  # white
+        elif (hue >= self.colours[0].minimum_hue) or (hue <= self.colours[0].maximum_hue):  # special case for red
+            return 'r'  # red
+        else:
             # check for orange, yellow, green and blue
             for colour_instance in self.colours[1:]:
-                if (hue >= colour_instance.minimum_hue) and (hue <= colour_instance.maximum_hue):  # the hue is within the colour instance's hue range
+                if (hue > colour_instance.minimum_hue) and (
+                        hue <= colour_instance.maximum_hue):  # the hue is within the colour instance's hue range
                     return colour_instance.colour  # the colour instance's colour name is returned
-
-        # checks for white if the checks for the other colours fail
-        if (saturation <= self.white_max_saturation) and (brightness >= self.white_min_brightness):
-            return 'w'
 
         # if the checks for red, orange, yellow, green, blue and white fail,
         #   it is unlikely for the dominant HSB colour to be any useful colour
